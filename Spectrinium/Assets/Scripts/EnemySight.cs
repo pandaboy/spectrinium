a033b7def7a915d3ce97d3fdb4e26245539700e3
sanoonan <sanoonan@tcd.ie>
2015-01-29 18:27:20 +0000

    Sprint 1 - Enemy View Cone
    Added simple enemy class, that can sense a player (object with the
    "Player" tag) in a view cone, and cast a ray to make sure no obstacles
    are in the way.  Includes a scene used for debugging.

---
+++ Spectrinium/Assets/Scripts/EnemySight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour
+{
+	public GameObject eyes;
+
+    void OnTriggerStay(Collider other)
+    {
+		Debug.Log ("sense something");
+        GameObject other_object = other.gameObject;
+
+        if (other_object.tag == "Player")
+		{
+            Debug.Log("sense player");
+
+			Vector3 player_position = other_object.transform.position;
+			Vector3 eye_position = eyes.transform.position;
+			Vector3 dir = player_position - eye_position;
+			RaycastHit hitInfo;
+
+			bool hit = Physics.Raycast(eye_position, dir.normalized, out hitInfo);
+
+			if(hit)
+			{
+				Debug.Log ("i can see something");
+				Collider coll = hitInfo.collider;
+				if(coll.gameObject.tag == "Player")
+					Debug.Log ("i can see the player");
+			}
+		}
+    }
+}
