6396d73abc22be8a7aaf7c9ceb54f38303001b17
sanoonan <sanoonan@tcd.ie>
2015-01-30 12:49:08 +0000

    Added script to handle enemy health and bullet response

---
+++ Spectrinium/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int max_health = 100;
+	public int current_health;
+
+
+	void Start()
+	{
+		current_health = max_health;
+	}
+
+
+	public void shot(int dec)
+	{
+		current_health -= dec;
+
+		if(current_health < 0)
+			current_health = 0;
+	}
+}
