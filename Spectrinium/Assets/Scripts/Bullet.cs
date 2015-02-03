5ae86854cc5c594921001a1aa85e07c07e6d95b0
Brendan O'Connor <sp.brendan@gmail.com>
2015-02-03 21:50:47 +0000

    Basic Bullet class and controller
    Created a simple class that handles a bullet (lifetime, damage, etc),
    and a support class that fires the bullets.

---
+++ Spectrinium/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// lifetime of the bullet
+	public float lifetime = 3.0f;
+	// damage the bullet does if it hits an active object
+	public int damage = 5;
+	
+	// update the bullet
+	void Update () {
+		lifetime -= Time.deltaTime;
+		
+		if(lifetime <= 0.0f) {
+			Destroy(gameObject);
+		}	
+	}
+	
+	void OnCollisionEnter(Collision other) {
+		// check if the other object is a player/enemy - hurt it if it is
+		if(other.gameObject.tag == "enemy") {
+			// deduct it's health if it has the EnemyHealth component
+			if(other.gameObject.GetComponent<EnemyHealth>() != null) {
+				other.gameObject.GetComponent<EnemyHealth>().shot(damage);
+			}
+		}
+		
+		// kill the bullet as soon as we hit the other object
+		// check if the other object is a player/enemy - hurt it if it is
+		if(other.gameObject.tag == "Environment" || other.gameObject.tag == "enemy") {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Spectrinium/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletManager : MonoBehaviour {
+
+	public Transform bullet;
+	public int forwardForce = 1000;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) {
+			Transform bulletInstance = Instantiate(bullet, transform.position, Quaternion.identity) as Transform;
+			bulletInstance.rigidbody.AddForce(transform.forward * forwardForce);
+		}
+	}
+}
