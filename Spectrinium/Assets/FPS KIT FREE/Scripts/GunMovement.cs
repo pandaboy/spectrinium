6fc61cf0a66b511203f12424b57f6dfd6a8a21c4
WaltXin <xiongx@tcd.ie>
2015-02-06 20:18:25 +0000

    Hand and Gun models

---
+++ Spectrinium/Assets/FPS KIT FREE/Scripts/GunMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GunMovement : MonoBehaviour {
+
+    public float MoveAmount = 1;
+    public float MoveSpeed = 2;
+
+    public GameObject GUN;
+
+    public float MoveOnX;
+    public float MoveOnY;
+
+    public Vector3 DefaultPos;
+    public Vector3 NewGunPos;
+
+	// Use this for initialization
+	void Start () {
+        DefaultPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveOnX = Input.GetAxis("Mouse X") * Time.deltaTime * MoveAmount;
+
+        MoveOnY = Input.GetAxis("Mouse Y") * Time.deltaTime * MoveAmount;
+
+        NewGunPos = new Vector3(DefaultPos.x + MoveOnX, DefaultPos.y + MoveOnY, DefaultPos.z);
+
+        GUN.transform.localPosition = Vector3.Lerp(GUN.transform.localPosition, NewGunPos, MoveSpeed * Time.deltaTime);
+	}
+}
