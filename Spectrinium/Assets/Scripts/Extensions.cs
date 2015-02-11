c857130cf27ee5a0241825024819a0404e09a15d
Brendan O'Connor <sp.brendan@gmail.com>
2015-02-11 13:44:51 +0000

    Basic Game Controller and Spectrum switcher
    Ultra basic enum and logic to switch between the current value using "R"
    and "T" keys.

---
+++ Spectrinium/Assets/Scripts/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+// common class for holding extension methods
+public static class ExtensionMethods {
+
+	public static T Next<T>(this T curr) where T : struct
+	{
+		// only do this for enums
+		if(!typeof(T).IsEnum)
+			throw new ArgumentException(string.Format("Argument {0} is not an Enum", typeof(T).FullName));
+		
+		T[] Arr = (T[])Enum.GetValues(curr.GetType());
+		int j = Array.IndexOf<T>(Arr, curr) + 1;
+		
+		return Arr.Length == j ? Arr[0] : Arr[j];
+	}
+	
+	public static T Prev<T>(this T curr) where T : struct
+	{
+		// only do this for enums
+		if(!typeof(T).IsEnum)
+			throw new ArgumentException(string.Format("Argument {0} is not an Enum", typeof(T).FullName));
+			
+		T[] Arr = (T[])Enum.GetValues(curr.GetType());
+		int j = Array.IndexOf<T>(Arr, curr) - 1;
+		
+		// if j is less than 0 (i.e. -1), loop round to the last element of the array
+		return (j < 0) ? Arr[(Arr.Length - 1)] : Arr[j];
+	}
+}
---
+++ Spectrinium/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	// available wavelengths
+	private enum Wavelength { RED, GREEN, BLUE };
+	
+	// tracks the current wavelength
+	private Wavelength currentWavelength;
+	
+	/**
+	 * Returns the current wavelength's int value
+	 */
+	public int getCurrentWavelength()
+	{
+		return (int)currentWavelength;
+	}
+	
+	/**
+	 * Returns the current wavelength as a string e.g. "RED"
+	 */
+	public string getCurrentWavelengthAsString()
+	{
+		return currentWavelength.ToString();
+	}
+	
+	/**
+	 * Moves the current wavelength one step forward.
+	 */
+	public void nextWavelength()
+	{
+		currentWavelength = currentWavelength.Next();
+	}
+	
+	/**
+	 * Moves the current wavelength one step back
+	 */
+	public void prevWavelength()
+	{
+		currentWavelength = currentWavelength.Prev();
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentWavelength = Wavelength.BLUE;
+		// Debug.Log ("Current Wavelength: " + getCurrentWavelengthAsString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T)) {
+			nextWavelength();
+			// Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
+		}
+		if(Input.GetKeyDown(KeyCode.R)) {
+			prevWavelength();
+			// Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
+		}
+	}
+}
