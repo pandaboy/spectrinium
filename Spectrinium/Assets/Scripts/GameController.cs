59d544a6e7ea154bcb12eaeacd5850c3b52c4159
Brendan O'Connor <sp.brendan@gmail.com>
2015-02-18 16:57:34 +0000

    Can build map from 2D arrays (jagged and multidimensional) - issue with wall placements.

--- Spectrinium/Assets/Scripts/GameController.cs
+++ Spectrinium/Assets/Scripts/GameController.cs
@@ -12,14 +12,63 @@ public class GameController : MonoBehaviour
 	private Map map;
 	
 	// create a 3x6 map
-	private int[,,] map_source= {
+	private int[,,] map_source = {
 		{ {0,0,1}, {0,0,0}, {0,0,0} },
 		{ {0,1,0}, {1,0,0}, {0,1,0} },
 		{ {1,0,0}, {0,0,1}, {0,1,0} },
 		{ {0,1,0}, {1,0,0}, {0,1,0} },
 		{ {0,0,1}, {0,0,1}, {0,1,0} },
-		{ {1,0,0}, {0,1,0}, {0,0,0} },
+		{ {1,0,0}, {0,1,0}, {0,0,0} }
 	};
+	
+	// create a 4x4 map with a red wall at each extent
+	private int[,,] extents = {
+		{ {1,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
+		{ {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+		{ {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+		{ {1,0,0}, {0,0,0}, {0,0,0}, {1,0,0} }
+	};
+	
+	// basic 3x3 map
+	private int[,,] simple = {
+		{ {1,0,0}, {0,0,0}, {0,1,0} },
+		{ {1,0,0}, {0,0,0}, {0,0,1} },
+		{ {1,0,0}, {0,0,0}, {0,1,0} }
+	};
+	
+	// create a 1x3 map - using a traditional, c-style array
+	private int[][][] jagged_extents = new int[][][] {
+		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 1, 0 } },
+		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 0, 1 } },
+		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 1, 0 } },
+	};
+
+	// Use this for initialization
+	void Start () {
+		// set the start wavelength
+		currentWavelength = Wavelength.BLUE;
+		
+		// create the map
+		//map = new Map(map_source);
+		//map = new Map(extents);
+		map = new Map(simple);
+		//map = new Map(jagged_extents);
+		Debug.Log ("Map created.");
+		
+		// spawn the player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T)) {
+			nextWavelength();
+			Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
+		}
+		if(Input.GetKeyDown(KeyCode.R)) {
+			prevWavelength();
+			Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
+		}
+	}
 	
 	/**
 	 * Returns the current wavelength's int value
@@ -51,31 +100,5 @@ public class GameController : MonoBehaviour
 	public void prevWavelength()
 	{
 		currentWavelength = currentWavelength.Prev();
-	}
-
-	// Use this for initialization
-	void Start () {
-		// set the start wavelength
-		currentWavelength = Wavelength.BLUE;
-		Debug.Log ("Current Wavelength: " + getCurrentWavelengthAsString());
-		
-		// create the map
-		// map = new Map(3, 3);
-		map = new Map(map_source);
-		Debug.Log ("Map created.");
-		
-		// spawn the player
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.T)) {
-			nextWavelength();
-			Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
-		}
-		if(Input.GetKeyDown(KeyCode.R)) {
-			prevWavelength();
-			Debug.Log("Current Wavelength: " + getCurrentWavelengthAsString());
-		}
 	}
 }
--- Spectrinium/Assets/Scripts/Map/Map.cs
+++ Spectrinium/Assets/Scripts/Map/Map.cs
@@ -4,45 +4,18 @@ using System.Collections;
 
 // basic Map object.
 public class Map
-{	
-	// Map Dimensions
-	int length = 3, width = 3;
+{
+	public GameController controller;
 	
-	float floor_y = 0;
+	private int length;
+	private int width;
+	private float floor_y = 0;
 	private float roof_y = 5;
-	float wall_height = 5;
+	private float wall_height = 5;
+	private float wall_thickness = 10;
 	
 	// 2D array of tiles
-	Tile[,] tiles;
-	
-	// Floor gameObject
-	GameObject floor;
-	
-	// TESTING
-	// red test array
-	bool[,] reds = new bool[,]{
-		{false, false, false},
-		{false, true, false},
-		{false, false, false},
-	};
-	
-	// greens test array
-	bool[,] greens = new bool[,]{
-		{false, false, true},
-		{false, false, true},
-		{false, true, true},
-	};
-	
-	// blues test array
-	bool[,] blues = new bool[,]{
-		{false, false, false},
-		{false, false, false},
-		{true, true, false},
-	};
-	
-	public Map() {
-		tiles = new Tile[length, width];
-	}
+	private Tile[,] tiles;
 	
 	public Map(int[,,] map_array) {
 		length = map_array.GetLength(0);
@@ -52,24 +25,28 @@ public class Map
 		BuildMap(map_array);
 	}
 	
-	public Map(int l, int w) {
-		length = l;
-		width = w;
+	public Map(int[][][] map_array) {
+		length = map_array.Length;
+		width = map_array[0].Length;
 		tiles = new Tile[length, width];
-		Debug.Log ("width: " + width + ",length: " + length);
-		BuildMap(reds, greens, blues);		
-		Debug.Log ("value at [1,1]: (" + GetTile (1,1).red + "," + GetTile(1,1).green + "," + GetTile(1,1).blue + ")");
-		Debug.Log ("Trying to build a floor");
-		BuildFloor();
+		
+		BuildMap(map_array);
 	}
 	
 	// builds the map using the passed arrays
-	bool BuildMap(bool[,] reds, bool[,] greens, bool[,] blues) {
+	bool BuildMap(int[][][] source) {
+		Debug.Log("This map of mine");
 		for(int i = 0; i < length; i++) {
 			for(int j = 0; j < width; j++) {
-				SetTile(i, j, reds[i,j], greens[i,j], blues[i,j]);
+				bool red = source[i][j][0] == 1 ? true : false;
+				bool green = source[i][j][1] == 1 ? true : false;
+				bool blue = source[i][j][2] == 1 ? true : false;
+				SetTile(i, j, red, green, blue);
 			}
 		}
+		BuildWalls();
+		BuildFloor();
+		BuildRoof();
 		
 		return true;
 	}
@@ -91,56 +68,60 @@ public class Map
 	}
 	
 	// build walls from the tiles array
-	void BuildWalls() {
-		Debug.Log ("Trying to build just white walls with the red source walls");
-		
+	public void BuildWalls()
+	{
 		for(int i = 0; i < length; i++) {
 			for(int j = 0; j < width; j++) {
 				Tile tile = tiles[i, j];
 				if(tile.red){
-					GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					wall.name = "Red Wall";
-					wall.tag = "Environment";
-					wall.transform.position = new Vector3(i, floor_y + (wall_height/2), j);
-					wall.transform.localScale = new Vector3(1.0f, wall.transform.localScale.y * wall_height, 1.0f);
-					wall.renderer.material.color = Color.red;
+					BuildWall ("red wall", "Environment", i, j, Color.red);
 				}
 				if(tile.green){
-					GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					wall.name = "Green Wall";
-					wall.tag = "Environment";
-					wall.transform.position = new Vector3(i, floor_y + (wall_height/2), j);
-					wall.transform.localScale = new Vector3(1.0f, wall.transform.localScale.y * wall_height, 1.0f);
-					wall.renderer.material.color = Color.green;
+					BuildWall ("green wall", "Environment", i, j, Color.green);
 				}
 				if(tile.blue){
-					GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					wall.name = "Blue Wall";
-					wall.tag = "Environment";
-					wall.transform.position = new Vector3(i, floor_y + (wall_height/2), j);
-					wall.transform.localScale = new Vector3(1.0f, wall.transform.localScale.y * wall_height, 1.0f);
-					wall.renderer.material.color = Color.blue;
+					BuildWall ("blue wall", "Environment", i, j, Color.blue);
 				}
 			}
 		}
 	}
 	
-	// builds the floor using Cube Primitives
-	public void BuildFloor() {
-		floor = BuildPlane("Floor", "Environment",
+	GameObject BuildWall(string name, string tag, int x, int z, Color color)
+	{
+		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		wall.name = name;
+		wall.tag = tag;
+		wall.renderer.material.color = color;
+		float x_offset = x - (length/2);
+		float z_offset = z - (width/2);
+		wall.transform.position = new Vector3(
+			x_offset * wall_thickness,
+			floor_y + (wall_height/2),
+			z_offset * wall_thickness
+		);
+		wall.transform.localScale = new Vector3(
+			wall_thickness,
+			wall.transform.localScale.y * wall_height,
+			wall_thickness
+		);
+		
+		return wall;
+	}
+	
+	void BuildFloor() {
+		BuildPlane("Floor", "Environment",
 			new Vector3(0.0F, floor_y, 0.0f),
-			new Vector3(width, 1.0F, length),
+			new Vector3(length, 1.0F, width),
 			Color.white
 		);
 	}
 	
-	// builds the floor using Cube Primitives
-	public void BuildRoof() {
-		GameObject roof = BuildPlane(
+	void BuildRoof() {
+		BuildPlane(
 			"Roof",
 			"Environment",
 			new Vector3(0.0F, roof_y, 0.0F),
-			new Vector3(width, -1.0F, length),
+			new Vector3(length, -1.0F, width), // flip the y-axis for the roof plane
 			Color.white
 		);
 	}
