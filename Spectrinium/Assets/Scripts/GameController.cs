f76629384087a094ee52061d6939918bf11401c6
Brendan O'Connor <sp.brendan@gmail.com>
2015-02-18 19:38:15 +0000

    Moving examples to their own file.

--- Spectrinium/Assets/Scripts/GameController.cs
+++ Spectrinium/Assets/Scripts/GameController.cs
@@ -11,77 +11,38 @@ public class GameController : MonoBehaviour
 	
 	private Map map;
 	public GameObject wall;
-	
-	// create a 3x6 map
-	private int[,,] map_source = {
-		{ {0,0,1}, {0,0,0}, {0,0,0} },
-		{ {0,1,0}, {1,0,0}, {0,1,0} },
-		{ {1,0,0}, {0,0,1}, {0,1,0} },
-		{ {0,1,0}, {1,0,0}, {0,1,0} },
-		{ {0,0,1}, {0,0,1}, {0,1,0} },
-		{ {1,0,0}, {0,1,0}, {0,0,0} }
-	};
-	
-	// create a 4x4 map with a red wall at each extent
-	private int[,,] extents = {
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
-		{ {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {1,0,0} }
-	};
-	
-	// create a 8x8 map with a red wall at each extent
-	private int[,,] big_extents = {
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {1,0,0} }
-	};
-	
-	// basic 3x3 map
-	private int[,,] simple = {
-		{ {1,0,0}, {0,0,0}, {0,1,0} },
-		{ {1,0,0}, {0,0,0}, {0,0,1} },
-		{ {1,0,0}, {0,0,0}, {0,1,0} }
-	};
-	
-	// GIGANTIC MAP
-	private int[,,] gigantic;
-	
-	// create a 1x3 map - using a traditional, c-style array
-	private int[][][] jagged_extents = new int[][][] {
-		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 1, 0 } },
-		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 0, 1 } },
-		new int[][] {new int[] {1, 0, 0 }, new int[] {0, 0, 0 }, new int[] {0, 1, 0 } },
-	};
 
 	// Use this for initialization
 	void Start () {
 		// set the start wavelength
 		currentWavelength = Wavelength.BLUE;
 		
-		gigantic = new int[10,10,3];
-		// build a gigantic map
+		// Example of an auto gen level
+		int[,,] auto = new int[10,10,3];
+		// build a gigantic map - this is a really poor map
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
 				for(int k = 0; k < 3; k++){
-					gigantic[i,j,k] = (j % 2 == 0 && i % 2 == 0) ? 1 : 0;
+					if(k == 0) {
+						auto[i,j,k] = (j % 2 == 0 && i % 2 == 0) ? 1 : 0;
+					}
+					if(k == 1) {
+						auto[i,j,k] = (j % 7 == 0 && i % 7 == 0) ? 1 : 0;
+					}
+					if(k == 2) {
+						auto[i,j,k] = (j % 5 == 0 && i % 5 == 0) ? 1 : 0;
+					}
 				}
 			}
 		}
 		
 		// create the map
-		//map = new Map(map_source);
-		//map = new Map(extents);
-		//map = new Map(big_extents);
-		map = new Map(gigantic, wall, getCurrentWavelengthAsString());
-		//map = new Map(simple);
-		//map = new Map(jagged_extents);
-		Debug.Log ("Map created.");
+		//map = new Map(EXAMPLES.map_source, wall, getCurrentWavelengthAsString());
+		map = new Map(EXAMPLES.extents, wall, getCurrentWavelengthAsString());
+		//map = new Map(EXAMPLES.big_extents, wall, getCurrentWavelengthAsString());
+		//map = new Map(EXAMPLES.simple, wall, getCurrentWavelengthAsString());
+		//map = new Map(EXAMPLES.jagged_extents, wall, getCurrentWavelengthAsString());
+		//map = new Map(auto, wall, getCurrentWavelengthAsString());
 		
 		// spawn the player
 	}
